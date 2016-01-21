8bb6d66385d67c90de6f754b39f3a9d0cb63ed27
Jared Hanson <jaredhanson@gmail.com>
2016-01-20 16:03:18 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# tokens-saml20
---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('tokens-saml20', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
